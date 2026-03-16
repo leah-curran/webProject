f6f4992d0a225f44e59de51abb0cbcc2eb51b38c
owennathanael <c00313648@setu.ie>
2026-03-16 11:20:01 +0000

    Refactor HTML structure and add navigation links

--- openCurrentAcc.html.php
+++ openCurrentAcc.html.php
@@ -3,104 +3,127 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="CurrentAcc.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="draft.css">
 </head>
 <body>
-    <h1>Amend/View a Student</h1>
-    <h4>Please Select a student and then click the amend button if you wish to update</h4>
-    <?php include 'CurrentAccList.php'; ?>
+	<div class="nav">
+     <img src="pics/logo.jpg" width="100" height="100">
+     <div class="links">
+    <ul><li><A HREF="MainMenu.html">Home</A></li>
+<li><A HREF="withdrawals.html">Withdrawals</A></li>
+<li><A HREF="Lodgements.html">Lodgements</A></li>
+<li><A HREF="CustomerMaintenance.html">Customer Maintenance</A></li>
+<li><A HREF="AccountMaintenance.html">Account Maintenance</A></li>
+<li><A HREF="Management.html">Management</A></li>
+<li><A HREF="Reports.html">Reports</A></li>
+<li><A HREF="Quotes.html">Quotes</A></li>
+<li><A HREF="ChangePassword.html">Change Password</A></li>
+</ul></div>
+    </div>
+    <div class="submenu">
+       <ul>
+        <P><a href=openCurrentAcc.html.php>Open CurrentAcc Menu</a></P>
+        <P><a href=AmendView_CurrentAcc.html.php>AmendView CurrentAcc Menu</a></P>
+       </ul> 
+    </div>
     <script>
-        //function to put the values
-        function populate()
-        {
-            var sel=document.getElementById("ListBox");
-            var result;
-            result=sel.options[sel.selectedIndex].value;
-            var personDetails=result.split(',');
-            document.getElementById("display").innerHTML="The details of the selected persons are: "+result;
-            document.getElementById("amendid").value=personDetails[0];
-            document.getElementById("amendBalance").value=personDetails[1];
-            document.getElementById("amendDateOpened").value=personDetails[2];
-            document.getElementById("amendAccStatus").value=personDetails[3];
-        }
-        //to togle between amend and view only
-        function toggleLock()
-        {
-            if (document.getElementById("amendViewbutton").value=="Amend Details")
-            {
-                document.getElementById("amendBalance").disabled=false;
-                document.getElementById("amendDateOpened").disabled=false;
-                document.getElementById("amendAccStatus").disabled=false;
-				document.getElementById("save").disabled=false;
-                document.getElementById("amendViewbutton").value="View Details";
-            }
-            else
-            {
-                document.getElementById("amendBalance").disabled=true;
-                document.getElementById("amendDateOpened").disabled=true;
-                document.getElementById("amendAccStatus").disabled=true;
-				document.getElementById("save").disabled=true;
-                document.getElementById("amendViewbutton").value="Amend Details";
-            }       
-        }
-        //check if the user actually wants to chance by using confirm
         function confirmCheck()
         {
             var response;
             response = confirm('Are you sure you want to save these changes?');
             if(response)
             {
-                document.getElementById("amendid").disabled=false;
-                document.getElementById("amendBalance").disabled=false;
-                document.getElementById("amendDateOpened").disabled=false;
-				document.getElementById("amendAccStatus").disabled=false;
-				document.getElementById("save").disabled=false;
-                document.getElementById("amendDOB").disabled=false;
                 return true;
             }
             else
             {
-                populate();
-				if(document.getElementById("amendViewbutton").value="View Details")
-				{
-                toggleLock();
-				}
                 return false;
             }
         }
-		function checkDate(input)
-        {
-            const inputDate= new Date(input.value);
-            const today=new Date();
-            if(inputDate>today)
-            {
-                input.setCustomValidity('Invalid Date, Cant be in the future');
-            }
-            else
-            {
-                input.setCustomValidity('');
-            }
-        }
     </script>
-    <p id= "display"> </p>
-    <input type ="button" value="Amend Details" id= "amendViewbutton" onclick="toggleLock()">
-    <form name="myForm" action="AmendView.php" onsubmit="return confirmCheck()" method="post">
-    <label for="amendid">Current Acc ID</label>
-    <input type="text" name="amendid" id="amendid" disabled>
-	<br><br>
-    <label for="amendBalance">Current Acc Balance</label>
-    <input type="text" name="amendBalance" id="amendBalance" pattern="[0-9*\,]+" disabled>
-		<br><br>
-    <label for="amendstudentaddress">Customer Address</label>
-    <input type="text" name="amendstudentaddress" id="amendstudentaddress" pattern="[A-Z*\s*\a-z*\0-9*\,*\]" disabled>
-		<br><br>
-	<label for="amendAccStatus">Account Status</label>
-    <input type="text" name="amendAccStatus" id="amendAccStatus" disabled>
-		<br><br>
-    <label for="amendDateOpened">Date of Birth</label>
-    <input type="date" name="amendDateOpened" id="amendDateOpened" max="<?php echo date('Y-m-d'); ?>" disabled>
-    <br><br>
-    <input type="submit" id="save" value="Save Changes" disabled>
-    </form>
+    <!-- this will move to the open current acc php file but will also run the confirm check function on submit -->
+<main>
+<div class = "currentAccBody">
+<h1>IF you arent registered yet you can go to this link</h1>
+<A HREF="addCustomer.html">Click Here</A>
+<br>
+<h1>Open a current Account</h1>
+<br>
+<h3>Search Customer</h3>
+<br>
+<label>1.Enter Customer ID
+<input type="number" id="cust_id_lookup" name="cust_id_lookup" />
+</label>
+<button type="button" onclick="lookupById()">Search</button>
+
+<br><br>
+
+<!-- Customer name dropdown populated from the database -->
+<label>2.Select Customer by Name
+<?php
+include 'db.inc.php';
+$sql = "SELECT CustId, Firstname, Surname, Phone, Email, eircode, dob FROM customer Where deleteFlag=0";
+if (!$result = mysqli_query($con, $sql))
+    {
+        die('Error in querying the database' . mysqli_error($con));
+    }
+echo "<select id='cust_select' onchange='lookupBySelect()'>";
+echo "<option value=''>-- Select a Customer --</option>";
+while ($row = mysqli_fetch_array($result))
+    {
+        $id      = $row['CustId'];
+        $fname   = $row['Firstname'];
+        $sname   = $row['Surname'];
+        $phone   = $row['Phone'];
+        $email   = $row['Email'];
+        $eircode = $row['eircode'];
+        $dob     = $row['dob'];
+        // Pack all the data into the value so JS can split it out
+        $allText = "$id,$fname,$sname,$phone,$email,$eircode,$dob";
+        echo "<option value='$allText'>$fname $sname</option>";
+    }
+echo "</select>";
+mysqli_close($con);
+?>
+</label>
+
+<br><br>
+
+<!-- Customer details shown here after lookup -->
+<div id="customerDetails" style="display:none;">
+<hr>
+<h3>Are the Details Correct?</h3>
+<br>
+<p><strong>Customer ID:</strong> <span id="disp_id"></span></p>
+<p><strong>Name:</strong> <span id="disp_name"></span></p>
+<p><strong>Phone:</strong> <span id="disp_phone"></span></p>
+<p><strong>Email:</strong> <span id="disp_email"></span></p>
+<p><strong>Eircode:</strong> <span id="disp_eircode"></span></p>
+<p><strong>Date of Birth:</strong> <span id="disp_dob"></span></p>
+<p><strong>What your Current Acc ID will be:</strong> <span id="disp_curID"></span></p>	
+<br>
+<p>If the details above are correct, please fill in the account details below.</p>
+<hr>
+<br>
+
+<!--Account details form -->
+<h3> Account Details</h3>
+<br>
+<form action="openCurrentAcc.php" method="post" onsubmit="return confirmCheck()">
+
+<!-- Hidden field to pass the customer ID into the form -->
+<input type="hidden" name="cust_id" id="cust_id" />
+<label>How much overdraft will you allow?
+    <input type="text" name="overdraft" id="overdraft" pattern="[0-9]+" required/>
+</label>
+<br><br>
+
+<button type="submit">Open Current Account</button>
+<script src="openCurrentAcc.js"></script>
+</form>
+</main>
+
 </body>
 </html>
+
